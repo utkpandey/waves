576b4cc071a03b191aff049319c749022e99432a
utkpandey <57761225+utkpandey@users.noreply.github.com>
2020-08-30 01:54:58 +0530

    vue setup
    setup done

--- waves/src/App.vue
+++ waves/src/App.vue
@@ -1,6 +1,6 @@
 <template>
  
-  <div>Hi Utkarsh</div>
+  <div>Hi Utkarsh 3</div>
 </template>
 
 <script>
@@ -15,30 +15,4 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
--- waves/src/main.js
+++ waves/src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './components/home-page/home-page.vue'
 
 new Vue({
   el: '#app',
