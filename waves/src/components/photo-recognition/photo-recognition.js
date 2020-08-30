594e193ea0d679a4449101a75b0503f4607bd6d7
utkpandey <57761225+utkpandey@users.noreply.github.com>
2020-08-31 02:59:53 +0530

    file-upload
    upload file done

--- waves/src/components/photo-recognition/photo-recognition.js
+++ waves/src/components/photo-recognition/photo-recognition.js
@@ -8,7 +8,33 @@ export default {
       return {
         expandNews: false,
         expandSingle: false,
-        amount: 99
+        amount: 99,
+        image: '',
+      }
+      
+    },
+    methods: {
+      onFileChange(e) {
+        var files = e.target.files || e.dataTransfer.files;
+        if (!files.length)
+          return;
+        this.createImage(files[0]);
+      },
+      createImage(file) {
+        var image = new Image();
+        var reader = new FileReader();
+        
+  
+        reader.onload = (e) => {
+          this.image = e.target.result;
+          
+        };
+        reader.readAsDataURL(file);
+        
+      },
+      removeImage: function (e) {
+        this.image = '';
+        
       }
     }
   }
