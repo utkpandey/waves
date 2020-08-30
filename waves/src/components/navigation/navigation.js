b9d7399a8f055f6790ebda230daafb2836f0f755
utkpandey <57761225+utkpandey@users.noreply.github.com>
2020-08-30 16:26:58 +0530

    nagivation
    navigation added

--- waves/src/components/navigation/navigation.js
+++ waves/src/components/navigation/navigation.js
@@ -2,12 +2,29 @@ export default {
     name: 'navigation',
     data () {
       return {
-        menuVisible: false
+        menuVisible: false,
+        OptionToDisplay:''
       }
     },
     methods: {
       toggleMenu () {
         this.menuVisible = !this.menuVisible
+      },
+      selectedNavigationOption(selectedValue){
+        switch (selectedValue) {
+          case 1:
+            this.OptionToDisplay='option 1'
+            break;
+          case 2:
+            this.OptionToDisplay='option 2'
+            break;
+          case 3:
+            this.OptionToDisplay='option 3'
+            break;
+            case 4:
+            this.OptionToDisplay='option 4'
+            break;
+        }
       }
     }
   }
--- waves/src/main.js
+++ waves/src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './components/home-page/home-page.vue'
 
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(VueMaterial)
+
 new Vue({
   el: '#app',
   render: h => h(App)
